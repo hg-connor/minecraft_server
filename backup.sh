92b9ef6aac4976f723fdadbb3335a9336213f379
Connor Hollis <connor@hemme.games>
2020-09-03 17:41:31 -0700

    updating backup script to use correct rcon syntax

--- backup.sh
+++ backup.sh
@@ -7,10 +7,10 @@ TIMESTAMP=$(date +$DATE_FORMAT)
 ARCHIVE_FILE_NAME=HEMMESERVERBACKUP_$TIMESTAMP.tar.gz
 ARCHIVE_PATH=$BACKUP_DIRECTORY/$ARCHIVE_FILE_NAME
 
-docker exec mc say Starting backup...
+docker exec mc rcon-cli say Starting backup...
 docker exec mc rcon-cli save-off
 
 tar -cvf $ARCHIVE_PATH -C $SERVER_DIRECTORY . --exclude *.jar
 
 docker exec mc rcon-cli save-on
-docker exec mc say Backup complete!
+docker exec mc rcon-cli say Backup complete!

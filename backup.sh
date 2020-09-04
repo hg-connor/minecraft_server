4afebed23047313d02081ea5830ede1c1863d292
Connor Hollis <connor@hemme.games>
2020-09-03 17:26:20 -0700

    Docker compose and backup script for minecraft server

---
+++ backup.sh
@@ -0,0 +1,16 @@
+BACKUP_DIRECTORY="/storage/minecraft-backup" # Where backups will go
+SERVER_DIRECTORY="/storage/minecraft-live" # Live game world directory
+
+DATE_FORMAT="%F_%H-%M-%S"
+TIMESTAMP=$(date +$DATE_FORMAT)
+
+ARCHIVE_FILE_NAME=HEMMESERVERBACKUP_$TIMESTAMP.tar.gz
+ARCHIVE_PATH=$BACKUP_DIRECTORY/$ARCHIVE_FILE_NAME
+
+docker exec mc say Starting backup...
+docker exec mc rcon-cli save-off
+
+tar -cvf $ARCHIVE_PATH -C $SERVER_DIRECTORY . --exclude *.jar
+
+docker exec mc rcon-cli save-on
+docker exec mc say Backup complete!

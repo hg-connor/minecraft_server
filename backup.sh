e109b065c89da6abebc0ef34bc0dc3f77e6a513c
root <root@ip-172-31-0-65.us-west-2.compute.internal>
2020-10-19 18:36:28 +0000

    Updating the backup script to prune old backup files

--- backup.sh
+++ backup.sh
@@ -7,10 +7,16 @@ TIMESTAMP=$(date +$DATE_FORMAT)
 ARCHIVE_FILE_NAME=HEMMESERVERBACKUP_$TIMESTAMP.tar.gz
 ARCHIVE_PATH=$BACKUP_DIRECTORY/$ARCHIVE_FILE_NAME
 
+# Disable mc server saving while we backup
 docker exec mc rcon-cli say Starting backup...
 docker exec mc rcon-cli save-off
 
+# Create the backup file
 tar -cvzf $ARCHIVE_PATH -C $SERVER_DIRECTORY . --exclude *.jar
 
+# Delete any backups older than 10 days to save space, we run volume snapshots daily so this should just keep the size down
+find $BACKUP_DIRECTORY* -mtime +10 -exec rm {} \;
+
+# Re-enable the save functionality for the MC server once the backup is complete
 docker exec mc rcon-cli save-on
 docker exec mc rcon-cli say Backup complete!
